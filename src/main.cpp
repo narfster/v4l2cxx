98c2f37bc53fcdc225d8f69bad9de1b9f9e3dc19
narfster <dan.yerushalmi@gmail.com>
2018-01-03 18:27:22 +0200

    Adding basic test sections to code.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <chrono>
 #include <thread>
 
+
+#define TEST_RUN (3)
+
 void callback_stdout_pipe(uint8_t *p_data, size_t len) {
 
     uint8_t outBuff[921600];
@@ -10,49 +13,56 @@ void callback_stdout_pipe(uint8_t *p_data, size_t len) {
     fwrite(outBuff, 921600, 1, stdout);
 }
 
+
 int main(){
 
-//    capture cap("/dev/video0", 640,480,pixel_format ::V4L2CXX_PIX_FMT_YUYV,callback_stdout_pipe);
-    capture cap("/dev/video0", callback_stdout_pipe);
-    cap.set_format(640,480,pixel_format ::V4L2CXX_PIX_FMT_YUYV);
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if (TEST_RUN == 1)
+    capture cap("/dev/video0", 640,480,pixel_format ::V4L2CXX_PIX_FMT_YUYV,callback_stdout_pipe);
     cap.run();
 
-//
-//    while(1){
-//        cap.read();
-//        std::this_thread::sleep_for(std::chrono::milliseconds(16));
-//    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+#elif (TEST_RUN == 2)
+    capture cap("/dev/video0", 640,480,pixel_format ::V4L2CXX_PIX_FMT_YUYV,callback_stdout_pipe);
+    while(1){
+        cap.read();
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
 
-}
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-#if 0
-int main() {
+#elif (TEST_RUN == 3)
 
     struct util_v4l2::buffer buffers[4];
 
-    error_code err_ = error_code::ERR_NO_ERROR;
+    error_code err = error_code::ERR_NO_ERROR;
 
-    int fd = util_v4l2::open_device("/dev/video0", &err_);
-    ASSERT_ERR_CODE(err_);
+    int fd = util_v4l2::open_device("/dev/video0", &err);
+    ASSERT_ERR_CODE(err);
 
-    auto cap = util_v4l2::query_capabilites(fd, &err_);
+    auto cap = util_v4l2::query_capabilites(fd, &err);
 
-    auto vec = util_v4l2::query_formats(fd, &err_);
+    auto vec = util_v4l2::query_formats(fd, &err);
 
-    util_v4l2::set_format(fd, 640, 480, pixel_format::V4L2CXX_PIX_FMT_YVYU, &err_);
+    util_v4l2::set_format(fd, 640, 480, pixel_format::V4L2CXX_PIX_FMT_YVYU, &err);
 
-    auto vec_format = util_v4l2::get_current_format(fd, &err_);
+    auto vec_format = util_v4l2::get_current_format(fd, &err);
 
     //util_v4l2::printv4l2_fmt(vec_format);
 
-    util_v4l2::init_mmap(fd, buffers, &err_);
+    util_v4l2::init_mmap(fd, buffers, &err);
 
-    util_v4l2::start_capturing(fd, 4, &err_);
+    util_v4l2::set_capture_steamon(fd,&err);
 
-    util_v4l2::mainloop(fd, buffers, callback_stdout_pipe);
+    util_v4l2::queue_frames(fd, 4, &err);
 
-    return 0;
-}
+    util_v4l2::mainloop(fd, buffers, callback_stdout_pipe,&err);
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
 #endif
+
+
+}
